ace96dde6479a089c569a3afa3cb8b372ff615f0
ashish kothari <ashish@blackcurrantapps.com>
2023-08-04 23:38:22 +0530

    practice

---
+++ index.js
@@ -0,0 +1,8 @@
+function Person (firstName, lastName, age) {
+    constructor (firstName, lastName, age) {
+        console.log('')
+    }
+}
+
+const x = new Person('ashish', 'kothari', '89')
+console.log(x.firstName)

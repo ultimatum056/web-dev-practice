b71c7543646a8dcafb21dcb8e819325a9cbf3c05
ashish kothari <ashish@blackcurrantapps.com>
2023-10-08 18:43:16 +0530

    practice javscript

--- index.js
+++ index.js
@@ -1,8 +1,75 @@
-function Person (firstName, lastName, age) {
-    constructor (firstName, lastName, age) {
-        console.log('')
+console.log('script has been executed')
+
+
+//factory function
+function createCircle(radius) {
+    return {
+        radius,
+        draw: function () {
+            console.log('-- draw')
+        }
     }
 }
 
-const x = new Person('ashish', 'kothari', '89')
-console.log(x.firstName)
+// console.log(createCircle)
+const circle = createCircle(1)
+// console.log('-- constructor', circle.constructor)
+
+// constructor function
+
+function Circle(radius) {
+    console.log('--this', this)
+    this.radius = radius;
+    this.draw = function () {
+        console.log('--draw')
+    }
+}
+
+
+const another = new Circle(1);
+
+// console.log('--another--', another)
+// console.log('--constructor', another.constructor)
+
+// inbuilt function constructor
+const Circle2 = new Function('radius', `
+this.radius = radius;
+this.draw = function () {
+    console.log('--draw Circle2 inbuilt function constructor', this.radius)
+}
+`)
+
+const circle3 = new Circle2(2);
+circle3.draw()
+
+// primitives are copied by value
+
+let p1 = 10;
+
+function inc(p1) {
+    p1++;
+}
+
+inc(p1);
+
+// console.log('--p1',p1);
+
+// objects are copied by reference
+
+let obj = {
+    value: 10,
+};
+
+function inc1(obj) {
+    obj.value++
+}
+
+inc1(obj)
+
+// console.log('--obj', obj)
+
+// iterating over object keys
+
+for (let key in circle3) {
+    console.log('--key', key, typeof key, typeof circle3[key])
+}
